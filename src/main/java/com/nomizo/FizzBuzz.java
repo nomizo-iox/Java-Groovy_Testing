7be31bef9f878c2c33ff0ebcf68e1e0a826de81e
nomizo-iox <nomizo.iox@gmail.com>
2020-04-18 08:34:37 -0500

    added a groovy file as a test class.

---
+++ src/main/java/com/nomizo/FizzBuzz.java
@@ -0,0 +1,9 @@
+package com.nomizo;
+
+public class FizzBuzz {
+    public String play(final int num) {
+        if(num % 3 == 0) return "Fizz";
+        if(num % 5 == 0) return "Buzz";
+        return "FizzBuzz";
+    }
+}
